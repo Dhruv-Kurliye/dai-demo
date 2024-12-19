70083070de4a519cfef55cafd91a836eb339aca8
Dhruv K <kurliyedrk@gmail.com>
2024-12-19 11:31:03 +0530

    multiplication function

--- Calculator.py
+++ Calculator.py
@@ -2,3 +2,4 @@ a= int(input("Enter the first number: "))
 b= int(input("Enter the second number: "))
 
 print("Addition of two numbers is : ", a+b)
+print("Multiplication of two numbers is : ", a*b)
