2c7334b089c368823b42edcb4cc5814464452f50
Dhruv K <kurliyedrk@gmail.com>
2024-12-19 12:05:05 +0530

    addition after reverting

--- Calculator.py
+++ Calculator.py
@@ -2,5 +2,3 @@ a= int(input("Enter the first number: "))
 b= int(input("Enter the second number: "))
 
 print("Addition of two numbers is : ", a+b)
-print("Multiplication of two numbers is : ", a*b)
-print("Subtraction of two numbers is : ", a-b)
